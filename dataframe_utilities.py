f52e10e6739ead501c67493418de81f43f91773c
RbChip <RbChip@localhost>
2018-08-24 15:52:59 -0400

    - Applied fix to replace_with_padding from mainline to be compatible with newer pandas

--- dataframe_utilities.py
+++ dataframe_utilities.py
@@ -159,14 +159,16 @@ def replace_with_padding(df,row,index):
         df = pad_columns(df, row.columns.nlevels)
     elif df.columns.nlevels > row.columns.nlevels:
         row = pad_columns(row, df.columns.nlevels)
-    # Wow, changing the index of a single row dataframe is a pain in
-    # the neck:
-    row = pandas.DataFrame(row.ix[0],columns=[index]).T
-    # Wow, replacing a row of a dataframe is a pain in the neck:
+
+    # Change the index of the row object to equal that of where it is to be
+    # inserted:
+    row.index = pandas.Int64Index([index])
+
+    # Replace the target row in the dataframe by dropping, appending, then
+    # sorting by index:
     df = df.drop([index])
     df = df.append(row)
     df = df.sort_index()
-    
     return df
 
 def dict_diff(dict1, dict2):
